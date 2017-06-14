0a9e46481e5393faf2265b7200ac000a7e8c16ca
David Wessman <david.wesmn@gmail.com>
2017-06-14 10:03:07 +0200

    Rspec instead of Test

--- test/controllers/inspections_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InspectionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get inspections_index_url
-    assert_response :success
-  end
-
-end
