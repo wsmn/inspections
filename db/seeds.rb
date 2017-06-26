693819cbfaa2c705e12dea229fffdb2cff6ea4b1
David Wessman <david.wesmn@gmail.com>
2017-06-26 18:09:20 +0200

    Rspec: setup sequences and methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..10).each do |i|
-  Customer.create(name: "David #{i}", phone: "070560788#{i}", address: "Home#{i}")
-end
+10.times { FactoryGirl.create(:customer) }
