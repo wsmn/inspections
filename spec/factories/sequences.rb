55eb39ad93fbd0b8228b2a52a373c70b30bad1ce
David Wessman <david.wesmn@gmail.com>
2017-06-26 18:09:20 +0200

    Rspec: setup sequences and methods

---
+++ spec/factories/sequences.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  sequence(:title) { |n| "#{['Titel', 'Rubrik', 'En titel'].sample} #{n}" }
+  sequence(:name) { |n| "#{%w[David Arthur Annika].sample} #{n}" }
+  sequence(:phone) { |n| "#{%w[07011111 07022222].sample}#{n}" }
+  sequence(:description) { |n| "A long description #{n}" }
+  sequence(:address) { |n| "#{road} #{n}, #{zipcode}" }
+
+  def road
+    %w[Briggvägen Brisvägen Brovägen].sample
+  end
+
+  def zipcode
+    %w[26376 26377 26361].sample
+  end
+end
