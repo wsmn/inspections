d89e18b3b5a9b8e6ff1c0862534066a4f6d894ad
David Wessman <david.wesmn@gmail.com>
2017-06-27 10:21:08 +0200

    Projects: changes views
    - More detailed menu
    - Add tile design to project#show

---
+++ app/helpers/project_helper.rb
@@ -0,0 +1,33 @@
+module ProjectHelper
+  def project_statuses
+    Project.statuses.keys.map { |k| [project_status(k), k] }
+  end
+
+  def project_status(key)
+    t("models.project.status.#{key}")
+  end
+
+  def project_title(status = nil)
+    content = [Project.model_name.human, project_tag(status)]
+    content_tag(:h1, safe_join(content), class: 'title is-1')
+  end
+
+  def project_tag(status, size: 'medium', pos: 'is-pulled-right')
+    return if status.nil?
+    cls = "tag #{project_status_class(status)} is-#{size} #{pos}"
+    content_tag(:span, project_status(status), class: cls)
+  end
+
+  def project_status_class(status)
+    case status
+    when 'active'
+      'is-info'
+    when 'paused'
+      'is-warning'
+    when 'completed'
+      'is-success'
+    when 'cancelled'
+      'is-danger'
+    end
+  end
+end
