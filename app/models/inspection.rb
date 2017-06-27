ca6382b13652a940bb1768073ab9e7a0b52eeb9e
David Wessman <david.wesmn@gmail.com>
2017-06-27 10:21:50 +0200

    Inspections and customers
    - Updates views  to use in new project layout

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -2,7 +2,12 @@ class Inspection < ApplicationRecord
   belongs_to(:project)
   has_one(:customer, through: :project)
 
-  validates(:on, presence: true)
+  validates(:at_date, presence: true)
 
-  scope(:by_date, -> { order(:on) })
+  scope(:by_date, -> { order(:at_date) })
+  scope(:upcoming, -> { by_date.where('at_date >= :time', time: Time.current) })
+
+  def to_s
+    I18n.l(at_date)
+  end
 end
