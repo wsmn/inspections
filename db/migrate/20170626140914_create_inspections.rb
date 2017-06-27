728fd1de36111470edc6d4805fcf69a530a7f7a6
David Wessman <david.wesmn@gmail.com>
2017-06-27 10:20:06 +0200

    Remove unused specs and change inspections

--- db/migrate/20170626140914_create_inspections.rb
+++ db/migrate/20170626140914_create_inspections.rb
@@ -2,7 +2,7 @@ class CreateInspections < ActiveRecord::Migration[5.1]
   def change
     create_table :inspections do |t|
       t.references :project, foreign_key: true, index: true, null: false
-      t.date :on, null: false
+      t.date :at_date, null: false
       t.integer :kind, null: false, default: 0
       t.integer :status, null: false, default: 0
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times { FactoryGirl.create(:customer) }
+10.times { FactoryGirl.create(:project) }
