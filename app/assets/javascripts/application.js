8caa48288290b355ccf5d3b9f855c80bcb1d7126
David Wessman <david.wesmn@gmail.com>
2017-07-03 15:08:28 +0200

    Setup Turbolinks and small changes
    - Add some global locales
    - Removes cable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require rails-ujs
-//= turbolinks
+//= require turbolinks
 //= require_tree .
