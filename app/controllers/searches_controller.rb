bb7a6824e679e1d1291aa4a22784d8457dcc5ae7
wsmn <arthurwessman@gmail.com>
2017-07-07 11:27:38 +0200

    Updated titles and search field for customers.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,10 @@ class SearchesController < ApplicationController
                        .text_search(search_param)
   end
 
+  def customer
+    @customers = Customer.text_search(search_param)
+  end
+
   private
 
   def search_param
