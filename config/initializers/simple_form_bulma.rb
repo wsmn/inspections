56521d1c349f617f83213962a6c2d902f317f0c1
David Wessman <david.wesmn@gmail.com>
2017-07-07 14:14:33 +0200

    Configure generators, remove style from bulma

--- config/initializers/simple_form_bulma.rb
+++ config/initializers/simple_form_bulma.rb
@@ -193,7 +193,6 @@ SimpleForm.setup do |config|
   config.wrapper_mappings = {
     check_boxes: :vertical_radio_and_checkboxes,
     radio_buttons: :vertical_radio_and_checkboxes,
-    file: :vertical_file_input,
     boolean: :vertical_boolean,
     datepicker: :vertical_datepicker,
     collection: :vertical_select,
