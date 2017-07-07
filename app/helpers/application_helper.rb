69bb5b0b1904498c1d35791b2b1f5c823fbd26ab
David Wessman <david.wesmn@gmail.com>
2017-07-07 14:14:14 +0200

    Update nav_item active classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-  def nav_is_active(css, controller)
-    css += ' is-active' if controller == controller_name
-    css
+  def nav_item_class(controllers, hidden: :mobile)
+    str = 'nav-item is-tab'
+    str += ' is-hidden-mobile' if hidden == :mobile
+    str += ' is-hidden-tablet' if hidden == :tablet
+    str += ' is-active' if nav_active?(controllers)
+    str
+  end
+
+  def nav_active?(controllers)
+    controllers.include?(controller_name)
   end
 end
