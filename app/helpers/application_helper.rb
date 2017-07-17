b0925def00714cef824722dc8c59d14a8da3b3fc
wsmn <arthurwessman@gmail.com>
2017-07-17 21:33:49 +0200

    Added time entries page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def nav_item_class(controllers, hidden: :desktop)
-    str = 'nav-item is-tab'
-    str += ' is-hidden-desktop' if hidden == :desktop
-    str += ' is-active' if nav_active?(controllers)
-    str
-  end
-
   def nav_active?(controllers)
     controllers.include?(controller_name)
   end
 
   def aside_item_class(controllers)
     'is-active' if nav_active?(controllers)
+  end
+
+  def navbar_is_active(css, controllers)
+    css += ' is-active' if nav_active?(controllers)
+    css
   end
 end
