f4c18136efb892404150da80d1c5f0a5ce822a86
David Wessman <david.wesmn@gmail.com>
2019-05-05 16:17:52 +0200

    Rails 5.2.3: remove react
    - Update to Webpacker 4
    - Cleanup yarn dependencies
    - Remove React
    - Remove Dropbox

--- app/javascript/packs/customers.jsx
+++
@@ -1,9 +0,0 @@
-import WebpackerReact from 'webpacker-react';
-import Customers from 'components/customers';
-import Customer from 'components/customer';
-import CustomerForm from 'components/customer_form';
-import CustomersTable from 'components/customers_table';
-
-WebpackerReact.setup({ Customer });
-WebpackerReact.setup({ CustomerForm });
-WebpackerReact.setup({ Customers });
--- app/javascript/packs/dropbox.jsx
+++
@@ -1,6 +0,0 @@
-import WebpackerReact from 'webpacker-react';
-import Turbolinks from 'turbolinks';
-import Dropbox from '../components/dropbox/dropbox';
-
-Turbolinks.start()
-WebpackerReact.setup({ Dropbox });
